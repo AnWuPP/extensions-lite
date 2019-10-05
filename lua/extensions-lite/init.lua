753317f8b352b7525bee82210046c019f7e18f2c
DonHomka <a.skinfy@gmail.com>
2019-10-05 10:53:26 +0300

    Bug fix

--- lua/extensions-lite/init.lua
+++ lua/extensions-lite/init.lua
@@ -1,19 +1,19 @@
 if string then
-   local stringex = require 'extensions.string'
+   local stringex = require 'extensions-lite.string'
 	for k, v in pairs(stringex) do
 		string[k] = v
 	end
 end
 
 if bit then
-	local bitex = require 'extensions.bit'
+	local bitex = require 'extensions-lite.bit'
 	for k, v in pairs(bitex) do
 		bit[k] = v
 	end
 end
 
 if table then
-	local tableex = require 'extensions.table'
+	local tableex = require 'extensions-lite.table'
 	for k, v in pairs(tableex) do
 		table[k] = v
 	end
